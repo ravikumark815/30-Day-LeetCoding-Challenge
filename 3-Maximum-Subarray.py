a9f2b8b1b9a99ead3a5b4fa4669197e38c4805d5
Ravi Kumar Reddy K <RaviKumarReddyK@sonicwall.com>
2020-04-08 15:18:56 +0530

    Added solution to Maximum Subarray problem of Day 3

---
+++ 3-Maximum-Subarray.py
@@ -0,0 +1,28 @@
+'''
+Question:
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Author: https://github.com/ravikumark815
+'''
+import os
+import sys
+
+def maxSubArray(nums):
+        subArraySum = nums[0]
+        resSum = nums[0]
+
+        for i in range(1,len(nums)):
+            subArraySum = max(subArraySum+nums[i], nums[i])
+            resSum = max(subArraySum, resSum)
+        return resSum
+
+if __name__ == "__main__":
+    liStr = input("\nProblem 3: Enter your list of integers separated by commas:")
+    li = []
+    li = liStr.split(",")
+    print("Result:\t",maxSubArray(li))
